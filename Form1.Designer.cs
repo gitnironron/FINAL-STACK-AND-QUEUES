b7eea0315dba126b1452bbc4b0e759e1a28ae67c
Ronilo P. Demetillo <Ronilo P. Demetillo@DESKTOP-9U7EMPD>
2024-12-15 22:13:41 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FINAL_STACK_AND_QUEUES
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button2 = new Button();
+            button3 = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Goldenrod;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(606, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(103, 44);
+            button2.TabIndex = 1;
+            button2.Text = "QUEUE";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Goldenrod;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(287, 228);
+            button3.Name = "button3";
+            button3.Size = new Size(103, 44);
+            button3.TabIndex = 2;
+            button3.Text = "STACK";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.Goldenrod;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnExit.Location = new Point(826, 440);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(103, 44);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "exit";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(960, 536);
+            Controls.Add(btnExit);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button2;
+        private Button button3;
+        private Button btnExit;
+    }
+}
---
+++ Queue.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace FINAL_STACK_AND_QUEUES
+{
+    partial class Queue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnQue = new Button();
+            btnDeq = new Button();
+            btnPeek = new Button();
+            textBox1 = new TextBox();
+            dgvQueue = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvQueue).BeginInit();
+            SuspendLayout();
+            // 
+            // btnQue
+            // 
+            btnQue.BackColor = Color.Goldenrod;
+            btnQue.FlatStyle = FlatStyle.Flat;
+            btnQue.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnQue.Location = new Point(135, 157);
+            btnQue.Name = "btnQue";
+            btnQue.Size = new Size(81, 37);
+            btnQue.TabIndex = 2;
+            btnQue.Text = "Queue";
+            btnQue.UseVisualStyleBackColor = false;
+            btnQue.Click += button2_Click;
+            // 
+            // btnDeq
+            // 
+            btnDeq.BackColor = Color.Goldenrod;
+            btnDeq.FlatStyle = FlatStyle.Flat;
+            btnDeq.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDeq.Location = new Point(244, 231);
+            btnDeq.Name = "btnDeq";
+            btnDeq.Size = new Size(81, 37);
+            btnDeq.TabIndex = 3;
+            btnDeq.Text = "dequeue";
+            btnDeq.UseVisualStyleBackColor = false;
+            btnDeq.Click += button1_Click_1;
+            // 
+            // btnPeek
+            // 
+            btnPeek.BackColor = Color.Goldenrod;
+            btnPeek.FlatStyle = FlatStyle.Flat;
+            btnPeek.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPeek.Location = new Point(136, 316);
+            btnPeek.Name = "btnPeek";
+            btnPeek.Size = new Size(81, 37);
+            btnPeek.TabIndex = 4;
+            btnPeek.Text = "Peek";
+            btnPeek.UseVisualStyleBackColor = false;
+            btnPeek.Click += btnPeek_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(129, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 31);
+            textBox1.TabIndex = 5;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // dgvQueue
+            // 
+            dgvQueue.BackgroundColor = Color.Goldenrod;
+            dgvQueue.BorderStyle = BorderStyle.Fixed3D;
+            dgvQueue.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvQueue.Columns.AddRange(new DataGridViewColumn[] { Column1 });
+            dgvQueue.GridColor = Color.Khaki;
+            dgvQueue.Location = new Point(586, 104);
+            dgvQueue.Name = "dgvQueue";
+            dgvQueue.RowHeadersWidth = 62;
+            dgvQueue.Size = new Size(166, 230);
+            dgvQueue.TabIndex = 6;
+            dgvQueue.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            Column1.HeaderText = "Queue";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Goldenrod;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Showcard Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(852, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 46);
+            button1.TabIndex = 13;
+            button1.Text = "menu";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_2;
+            // 
+            // Queue
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.hhhuigf;
+            ClientSize = new Size(959, 539);
+            Controls.Add(button1);
+            Controls.Add(dgvQueue);
+            Controls.Add(textBox1);
+            Controls.Add(btnPeek);
+            Controls.Add(btnDeq);
+            Controls.Add(btnQue);
+            Name = "Queue";
+            Text = "Queue";
+            Load += Queue_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvQueue).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnQue;
+        private Button btnDeq;
+        private Button btnPeek;
+        private TextBox textBox1;
+        private DataGridView dgvQueue;
+        private DataGridViewTextBoxColumn Column1;
+        private Button button1;
+    }
+}
